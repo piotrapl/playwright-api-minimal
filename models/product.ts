aa2adcad8b8d7df7b6ff4a1b254d59b8777b9afc
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-14 15:00:10 +0200

    komentarz nt modelu danych

--- models/product.ts
+++ models/product.ts
@@ -1,3 +1,7 @@
+// to jest model danych dla produktu, który jest używany w testach API.
+// bez niego nie mielibyśmy jasno zdefiniowanej struktury danych, 
+// co utrudniałoby pisanie testów
+
 export type Product = {
   id: number;
   title: string;
