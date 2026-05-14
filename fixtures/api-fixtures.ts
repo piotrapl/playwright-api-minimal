e6091afd6de862dd2e90051c9c868500431c16a4
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-14 14:49:28 +0200

    komantarz -nt. rozszerzenia klasy test

--- fixtures/api-fixtures.ts
+++ fixtures/api-fixtures.ts
@@ -1,6 +1,17 @@
 import { test as base } from '@playwright/test';
 import { ApiClient } from '../utils/api-client';
 
+/*
+ test = base.extend({ 
+ - to rozszerzenie funkcjonalności testów Playwright o dodatkowe możliwości, 
+   tu o klienta API.
+ api: async ({ request }, use) => { 
+ - funkcja, która tworzy instancję ApiClient i przekazuje do niej Playwright'owy obiekt request 
+ - ApiClient - klasa prawdopodobnie zawierająca metody do wykonywania zapytań HTTP..
+
+ await use(client) - użycie tej instancji klienta w testach
+ test - oznacza rozszerzony test, który będzie miał dostęp do klienta API
+*/ 
 export const test = base.extend({
   api: async ({ request }, use) => {
     const client = new ApiClient(request, {
