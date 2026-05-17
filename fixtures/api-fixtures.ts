f3c0e4d4443ae42bd41567535e544fd64c7feb40
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-05-17 16:13:30 +0200

    przeniesienie komentarzy na koniec pliku

--- fixtures/api-fixtures.ts
+++ fixtures/api-fixtures.ts
@@ -1,17 +1,6 @@
 import { test as base } from '@playwright/test';
 import { ApiClient } from '../utils/api-client';
 
-/*
- test = base.extend({ 
- - to rozszerzenie funkcjonalności testów Playwright o dodatkowe możliwości, 
-   tu o klienta API.
- api: async ({ request }, use) => { 
- - funkcja, która tworzy instancję ApiClient i przekazuje do niej Playwright'owy obiekt request 
- - ApiClient - klasa prawdopodobnie zawierająca metody do wykonywania zapytań HTTP..
-
- await use(client) - użycie tej instancji klienta w testach
- test - będzie oznaczał rozszerzoną klasę test, której obiekt będzie miał dostęp do klienta API
-*/ 
 export const test = base.extend({
   api: async ({ request }, use) => {
     const client = new ApiClient(request, {
@@ -23,3 +12,15 @@ export const test = base.extend({
 });
 
 export const expect = test.expect;
+
+/*
+ test = base.extend({ 
+ - to rozszerzenie funkcjonalności testów Playwright o dodatkowe możliwości, 
+   tu o klienta API.
+ api: async ({ request }, use) => { 
+ - funkcja, która tworzy instancję ApiClient i przekazuje do niej Playwright'owy obiekt request 
+ - ApiClient - klasa prawdopodobnie zawierająca metody do wykonywania zapytań HTTP..
+
+ await use(client) - użycie tej instancji klienta w testach
+ test - będzie oznaczał rozszerzoną klasę test, której obiekt będzie miał dostęp do klienta API
+*/ 
